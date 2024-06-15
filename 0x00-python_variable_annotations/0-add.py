689ee4163e55c2dd1704e5ab33df1937db8abe72
root <omar.elmishtawy@gmail.com>
2024-06-15 05:56:41 -0700

    cs

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,7 +5,7 @@ This is a function to add two variables
 
 
 def add(a: float, b: float)-> float:
-"""
-function two add two float integers and return a float
-"""
+    """
+    function two add two float integers and return a float
+    """
     return a + b
