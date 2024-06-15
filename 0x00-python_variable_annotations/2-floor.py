1432238d094d823f7b0477e6eceaacf2e85ed51f
root <omar.elmishtawy@gmail.com>
2024-06-15 06:04:31 -0700

    cs

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This is a function concat two strings
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    function to concat two strings
+    """
+    return math.floor(n)
