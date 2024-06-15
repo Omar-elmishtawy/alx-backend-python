f9d522c35db1e92acf0aa9063c50858b4a2e4013
root <omar.elmishtawy@gmail.com>
2024-06-15 06:02:21 -0700

    concat str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This is a function concat two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function to concat two strings
+    """
+    return str1 + str2
