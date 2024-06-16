55548d3a5241a2070090fe5947c7c7be3156073d
root <omar.elmishtawy@gmail.com>
2024-06-16 10:15:45 -0700

    fi

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """
 Module to basic async
-""" 
+"""
 
 
-import asyncio, random
+import asyncio
+import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """
